330b5e6265b737791d76cd475031bd4115c07949
pedrosdl <pedrotiagoluz@gmail.com>
2025-10-22 23:02:58 -0300

    Create index.php with basic HTML structure

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <title>PHP-Desktop</title>
+</head>
+<body>
+<div align="center">
+  <br/>
+<?php
+echo("Hello, Word");
+?>
+</div>
+</body>
+</html>
